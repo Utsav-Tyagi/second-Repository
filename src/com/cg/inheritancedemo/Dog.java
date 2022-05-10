8d65549a88018fda92ef6376e7e8a484776860a4
Utsav-Tyagi <utsavtyagi222@gmail.com>
2022-05-10 15:00:03 +0530

    third commit

--- src/com/cg/inheritancedemo/Dog.java
+++
@@ -1,9 +0,0 @@
-package com.cg.inheritancedemo;
-
-public class Dog extends Animal{
-	
-	public void eat() {
-		System.out.println("Dog eats Flesh");
-	}
-
-}
--- src/com/cg/inheritancedemo/PuppyDog.java
+++
@@ -1,9 +0,0 @@
-package com.cg.inheritancedemo;
-
-public class PuppyDog extends Dog{
-	
-	public void eat() {
-		System.out.println("Puppy dog eats biscuts");
-	}
-
-}
